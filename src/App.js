56e0cacd1762f9221ec1f456bff96de58dadac9f
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-01-23 12:54:39 +0300

    build files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import AdminPanel from "./components/AdminPanel";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminPanel from './components/AdminPanel';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Products from './pages/Products';
+import AddProduct from './pages/AddProduct';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
-    <div>
-      <AdminPanel />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/checkout" element={<Checkout />} />
+
+          {/* Admin routes */}
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/add-product" element={<AddProduct />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

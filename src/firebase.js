44356ffa1fd3d88deef437200a4f8704ddb09236
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-01-23 12:13:46 +0300

    user page

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage"; // Import Storage
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAO1prGq3oQiJ2P92SwCF3L3QlcwHJY2To",
   authDomain: "green-market-7872b.firebaseapp.com",
   projectId: "green-market-7872b",
-  storageBucket: "green-market-7872b.firebasestorage.app",
+  storageBucket: "green-market-7872b.appspot.com", // Corrected storageBucket URL
   messagingSenderId: "659801733226",
-  appId: "1:659801733226:web:697fc24a22884ce2fa3213"
+  appId: "1:659801733226:web:697fc24a22884ce2fa3213",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
+export const storage = getStorage(app); // Initialize Storage

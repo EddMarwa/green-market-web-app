6b06cdee775adf965e1b876aacffdb11aa32506d
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-01-23 11:48:58 +0300

    admin panel

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UploadImage from "./UploadImage";
+
+const AdminPanel = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <UploadImage />
+    </div>
+  );
+};
+
+export default AdminPanel;
